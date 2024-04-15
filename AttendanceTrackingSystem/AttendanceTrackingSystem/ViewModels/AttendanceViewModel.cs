bdea6cf1d01b9da327f4acc1d026b8b49cfb4a33
EbrahimDawoud <ebrahim_EN30895331@f-eng.tanta.edu.eg>
2024-04-15 21:23:19 +0200

    add Attendance

---
+++ AttendanceTrackingSystem/AttendanceTrackingSystem/ViewModels/AttendanceViewModel.cs
@@ -0,0 +1,10 @@
+﻿using AttendanceTrackingSystem.Models;
+
+namespace AttendanceTrackingSystem.ViewModels
+{
+    public class AttendanceViewModel
+    {
+        public List<User> Present { get; set; }
+        public List<User> Absent { get; set; }
+    }
+}
